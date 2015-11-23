aa537f05dfde0ea5969ce8911a5ee4b7f30ad31b
jiantao88 <a_tao123@163.com>
2015-11-23 09:53:03 +0800

    iwork 新浪微博申请

--- app/src/main/java/com/iwork/ui/activity/MainActivity.java
+++ app/src/main/java/com/iwork/ui/activity/MainActivity.java
@@ -1,17 +1,26 @@
 package com.iwork.ui.activity;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.impetusconsulting.iwork.R;
+import com.qiniu.android.utils.StringUtils;
+
+import java.util.Iterator;
+import java.util.List;
 
 import cn.jpush.android.api.JPushInterface;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 import cn.smssdk.SMSSDK;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,10 +36,34 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                OnekeyShare oks = new OnekeyShare();
+                oks.setText("我是分享文本");
+                oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/05/21/oESpJ78_533x800.jpg");
+                // 启动分享GUI
+                oks.show(MainActivity.this);
+
             }
         });
+    }
+
+    private byte[] getSign(Context context) {
+        PackageManager pm = context.getPackageManager();
+        List<PackageInfo> apps = pm
+                .getInstalledPackages(PackageManager.GET_SIGNATURES);
+        Iterator<PackageInfo> iter = apps.iterator();
+
+        while (iter.hasNext()) {
+            PackageInfo info = iter.next();
+            String packageName = info.packageName;
+            //按包名 取签名
+            if (packageName.equals("com.impetusconsulting.iwork")) {
+                return info.signatures[0].toByteArray();
+
+            }
+        }
+        return null;
     }
 
     @Override
