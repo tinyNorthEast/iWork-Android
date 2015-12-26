bdea4215b66894eb65d3aea3d2e09188fc17a837
jiantao88 <a_tao123@163.com>
2015-12-26 08:21:03 +0800

    详情页

--- app/src/main/java/com/iwork/ui/activity/MessageActivity.java
+++ app/src/main/java/com/iwork/ui/activity/MessageActivity.java
@@ -4,8 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.impetusconsulting.iwork.R;
+import com.iwork.Base.BaseActivity;
 
-public class MessageActivity extends AppCompatActivity {
+public class MessageActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
