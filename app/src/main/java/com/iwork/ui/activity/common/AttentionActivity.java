8526119c54ecf3a756363c0c2827b3c2feadc165
jiantao88 <a_tao123@163.com>
2016-01-31 23:34:38 +0800

    修改关注列表图片bug

--- app/src/main/java/com/iwork/ui/activity/common/AttentionActivity.java
+++ app/src/main/java/com/iwork/ui/activity/common/AttentionActivity.java
@@ -1,9 +1,7 @@
 package com.iwork.ui.activity.common;
 
-import android.graphics.Canvas;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.impetusconsulting.iwork.R;
@@ -12,10 +10,8 @@ import com.iwork.adapter.recyclerview.BaseAdapterHelper;
 import com.iwork.adapter.recyclerview.QuickAdapter;
 import com.iwork.helper.ToastHelper;
 import com.iwork.model.AttentionListModel;
-import com.iwork.model.CommentListModel;
 import com.iwork.net.CommonRequest;
 import com.iwork.okhttp.callback.ResultCallback;
-import com.iwork.ui.view.DividerItemDecoration;
 import com.iwork.ui.view.TitleBar;
 import com.iwork.utils.CollectionUtil;
 import com.iwork.utils.Constant;
@@ -25,7 +21,6 @@ import com.jcodecraeer.xrecyclerview.ProgressStyle;
 import com.jcodecraeer.xrecyclerview.XRecyclerView;
 import com.squareup.okhttp.Request;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -37,7 +32,7 @@ public class AttentionActivity extends BaseActivity {
     @Bind(R.id.attention_titlebar)
     TitleBar attentionTitlebar;
     private int searchType;
-    private int pageNo=1;
+    private int pageNo = 1;
     private int pageSize = 10;
     @Bind(R.id.attention_xrecyclerview)
     XRecyclerView attentionXrecyclerview;
@@ -70,11 +65,11 @@ public class AttentionActivity extends BaseActivity {
             @Override
             public void onResponse(AttentionListModel response) {
                 if (response.getInfoCode() == 0) {
-                    if (CollectionUtil.isEmpty(response.getData())){
+                    if (CollectionUtil.isEmpty(response.getData())) {
                         ToastHelper.showShortInfo("当前没有消息");
                         return;
                     }
-                    attentions=response.getData();
+                    attentions = response.getData();
                     initAdapter();
                 }
             }
@@ -126,7 +121,9 @@ public class AttentionActivity extends BaseActivity {
             protected void convert(BaseAdapterHelper helper, AttentionListModel.Attention item) {
                 helper.getTextView(R.id.attention_name_tv).setText(item.getZh_name());
                 helper.getTextView(R.id.attention_time_tv).setText(TimeUtil.formateTimeMMddHHmm(item.getCreate_time()));
-                Glide.with(AttentionActivity.this).load(item.getPic()).into(helper.getImageView(R.id.attention_img));
+                Glide.with(AttentionActivity.this).load(item.getPic()).centerCrop()
+                        .error(R.drawable.detail_no_pic).placeholder(R.drawable.detail_no_pic)
+                        .into(helper.getImageView(R.id.attention_img));
             }
         };
         attentionXrecyclerview.setAdapter(mAdapter);
